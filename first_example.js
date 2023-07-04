acbf9231ddce39f6e2dd1719fdae49e85336793e
Ângela Barros <angela.barros@codacy.com>
2023-07-04 15:01:54 +0100

    first commit

---
+++ first_example.js
@@ -0,0 +1,24 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+console.log(addNumbers(5, 10));
+
+const myArray = [1, 2, 3];
+myArray.push(4);
+console.log(myArray);
+
+let x = 5;
+x = 10;
+console.log(x);
+
+const person = {
+  name: "John",
+  age: 25,
+};
+
+person.age = 30;
+console.log(person);
+
+const result = 10 / "a";
+console.log(result);
