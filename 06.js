38be6b5c1b5fa376ebe4b80dde18155c4f566fb5
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-07-27 16:19:03 +0200

    fix 5 6 8

--- 06.js
+++ 06.js
@@ -1,7 +1,7 @@
 function sumArray(arr) {
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] == 0) {
+        if (parseInt(arr[i]) !== 0) {
             sum += arr[i];
         }
     }
