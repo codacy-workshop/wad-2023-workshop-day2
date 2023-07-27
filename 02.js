984f061ab93ab6b98c5f9d53ffe5bbf957d40c89
Willi Meierhof <Willi.Meierhof@tracetronic.de>
2023-07-27 16:08:21 +0200

    fix 2

--- 02.js
+++ 02.js
@@ -4,23 +4,27 @@ function getSeason(month) {
         case 2:
         case 12:
             console.log("It's winter.");
+            break;
         case 3:
         case 4:
         case 5:
             console.log("It's spring.");
+            break;
         case 6:
         case 7:
         case 8:
             console.log("It's summer.");
+            break;
         case 9:
         case 10:
         case 11:
             console.log("It's autumn.");
+            break;
         default:
             console.log("Invalid month number.");
     }
 }
 
+var x = 2;
 getSeason(x);
 
-var x = 20;
