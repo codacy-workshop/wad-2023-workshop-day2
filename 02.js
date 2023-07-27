5c6ef3dab57c04f649f20cc6a36674643454e5f5
Fedor Novikov <fedor.novikov@bolt.eu>
2023-07-27 16:39:11 +0200

    fixes

--- 02.js
+++ 02.js
@@ -1,28 +1,14 @@
 function getSeason(month) {
-    switch (month) {
-        case 1:
-        case 2:
-        case 12:
-            console.log("It's winter.");
-            return;
-        case 3:
-        case 4:
-        case 5:
-            console.log("It's spring.");
-            return;
-        case 6:
-        case 7:
-        case 8:
-            console.log("It's summer.");
-            return;
-        case 9:
-        case 10:
-        case 11:
-            console.log("It's autumn.");
-            return;
-        default:
-            console.log("Invalid month number.");
-            return;
+    if (month === 1 || month === 2 || month === 12) {
+        console.log("It's winter.");
+    } else if (month === 3 || month === 4 || month === 5) {
+        console.log("It's spring.");
+    } else if (month === 6 || month === 7 || month === 8) {
+        console.log("It's summer.");
+    } else if (month === 9 || month === 10 || month === 11) {
+        console.log("It's autumn.");
+    } else {
+        console.log("Invalid month number.");
     }
 }
 
