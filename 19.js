51b461304993f497c68e308a2ccc7aebb0d3eb80
Stanislau Karaban <stanislau@parloa.com>
2023-07-27 16:28:44 +0200

    19

--- 19.js
+++ 19.js
@@ -1,33 +1,20 @@
 function determineWeatherCondition(weatherData) {
     if (weatherData.temperature > 30) {
         if (weatherData.humidity > 80) {
-            if (weatherData.windSpeed < 10) {
-                return "Hot and humid with light breeze.";
-            } else {
-                return "Hot and humid with strong wind.";
-            }
-        } else {
-            if (weatherData.windSpeed < 10) {
-                return "Hot and dry with light breeze.";
-            } else {
-                return "Hot and dry with strong wind.";
-            }
-        }
-    } else {
-        if (weatherData.humidity > 80) {
-            if (weatherData.windSpeed < 10) {
-                return "Cool and humid with light breeze.";
-            } else {
-                return "Cool and humid with strong wind.";
-            }
-        } else {
-            if (weatherData.windSpeed < 10) {
-                return "Cool and dry with light breeze.";
-            } else {
-                return "Cool and dry with strong wind.";
-            }
-        }
+            if (weatherData.windSpeed < 10) return "Hot and humid with light breeze.";
+            return "Hot and humid with strong wind.";
+        } 
+        if (weatherData.windSpeed < 10) return "Hot and dry with light breeze.";
+        return "Hot and dry with strong wind.";
     }
+    
+    if (weatherData.humidity > 80) {
+            if (weatherData.windSpeed < 10) return "Cool and humid with light breeze.";
+            return "Cool and humid with strong wind.";
+    }
+
+    if (weatherData.windSpeed < 10) return "Cool and dry with light breeze.";
+    return "Cool and dry with strong wind.";
 }
 
 // Example
