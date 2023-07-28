2411b4884687729f15f1fc76a0c03a84cc9a5c26
adrianbunea <adrian.bunea28@gmail.com>
2023-07-28 14:49:38 +0300

    Fix 04

--- 04.js
+++ 04.js
@@ -1,12 +1,8 @@
 var nums = [];
 
-function createNumberFunctions() { 
-    var temp;
-
-    for (i = 0; i < 10; i++) {
-        nums[i] = function (j) {
-            return i + j;
-        };
+function createNumberFunctions() {
+    for (let i = 0; i < 10; i++) {
+        nums[i] = (j) => i + j
     }
 
     return nums;
