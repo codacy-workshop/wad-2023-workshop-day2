a4c98036b29f4c97558ca89ad0fe051ced554d7d
Bassel Hossam <basselhossam.a@gmail.com>
2023-07-28 14:12:39 +0200

    Fix 17.js

--- 17.js
+++ 17.js
@@ -7,7 +7,7 @@ function functionB() {
 }
 
 function functionChecker(funcA, funcB) {
-    if (typeof funcA == "funcion" && typeof funcB == "function") {
+    if (typeof funcA === 'function' && typeof funcB === 'function') {
         console.log("Both are functions");
     } else {
         console.log("One object is not a function");
