9e9e2feca3b8318960da86d7281ca65a6aec6b3c
Vadim Shevchenko <veejar.net@gmail.com>
2023-07-28 13:48:10 +0200

    Fixed 01.js and 02.js

--- 02.js
+++ 02.js
@@ -1,26 +1,15 @@
 function getSeason(month) {
-    switch (month) {
-        case 1:
-        case 2:
-        case 12:
-            console.log("It's winter.");
-        case 3:
-        case 4:
-        case 5:
-            console.log("It's spring.");
-        case 6:
-        case 7:
-        case 8:
-            console.log("It's summer.");
-        case 9:
-        case 10:
-        case 11:
-            console.log("It's autumn.");
-        default:
-            console.log("Invalid month number.");
+    if (month === 1 || month === 2 || month === 12){
+        console.log("It's winter.");
+    } else if (month >= 3 && month <= 5) {
+        console.log("It's spring.");
+    }  else if (month >= 6 && month <= 8) {
+        console.log("It's summer.");
+    } else if (month >= 9 && month <= 12) {
+        console.log("It's autumn.");
+    } else {
+        console.log("Invalid month number.");
     }
 }
 
 getSeason(x);
-
-var x = 20;
