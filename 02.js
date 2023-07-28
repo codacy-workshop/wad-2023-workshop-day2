202e5792a072cee824ae3500f12e53ff5bf8ba06
Pedro Diogo <p.diogo@eyeo.com>
2023-07-28 14:11:07 +0200

    solve 2 again

--- 02.js
+++ 02.js
@@ -3,15 +3,19 @@ function getSeason(month) {
     if (month === 1 || month === 2 || month === 12) {
         message = "It's winter.";
     }
-    else if (month === 3 || month === 4 || month === 5) {
+    
+    if (month === 3 || month === 4 || month === 5) {
         message = "It's spring.";
     }
-    else if (month === 6 || month === 7 || month === 8) {
+    
+    if (month === 6 || month === 7 || month === 8) {
         message = "It's summer.";
     }
-    else if (month == 9 || month === 10 || month === 11) {
+    
+    if (month === 9 || month === 10 || month === 11) {
         message = "It's autumn.";
     }
+
     console.log(message);
 }
 
