9bc5e59ad9acbc1c8e2d313126ede2a281e94374
Bassel Hossam <basselhossam.a@gmail.com>
2023-07-28 13:40:04 +0200

    Fix 04.js

--- 04.js
+++ 04.js
@@ -1,9 +1,6 @@
-var nums = [];
-
-function createNumberFunctions() { 
-    var temp;
-
-    for (i = 0; i < 10; i++) {
+function createNumberFunctions() {
+    let nums = [];
+    for (let i = 0; i < 10; i++) {
         nums[i] = function (j) {
             return i + j;
         };
