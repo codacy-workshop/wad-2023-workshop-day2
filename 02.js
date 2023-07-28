21f53e2b04416328ecf152bf7ba9bd2935e18d9e
Ahmed Abdelhady <ahmed.abdelhady@bolt.eu>
2023-07-28 13:35:41 +0200

    Fixed 01 and 02

--- 02.js
+++ 02.js
@@ -4,18 +4,22 @@ function getSeason(month) {
         case 2:
         case 12:
             console.log("It's winter.");
+            return;
         case 3:
         case 4:
         case 5:
             console.log("It's spring.");
+            return;
         case 6:
         case 7:
         case 8:
             console.log("It's summer.");
+            return;
         case 9:
         case 10:
         case 11:
             console.log("It's autumn.");
+            return;
         default:
             console.log("Invalid month number.");
     }
