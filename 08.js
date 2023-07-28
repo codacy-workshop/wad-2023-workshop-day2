95d2880be9dfe6e255e50f8085b543e6b9ceed93
Bassel Hossam <basselhossam.a@gmail.com>
2023-07-28 13:50:07 +0200

    Fix 08.js

--- 08.js
+++ 08.js
@@ -1,8 +1,7 @@
 function calculateArea(radius) {
-    var pi = 3.141592653589793238462643383279502884197;
-    var area = pi * radius * radius;
-    return area;
+    const pi = Math.PI;
+    return  pi * radius * radius;
 }
 
-var circleRadius = 5;
+let circleRadius = 5;
 console.log("Area of the circle:", calculateArea(circleRadius));
