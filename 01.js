9a35d5cd57a6de20b3b5fbd676c38de2ccca5218
Juan Carvajal <>
2023-07-28 13:58:48 +0200

    Fixing issue 01

--- 01.js
+++ 01.js
@@ -1,5 +1,5 @@
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 == 0 && i % 5 == 0) {
+  if (i % 3 === 0 && i % 5 === 0) {
     console.log("FizzBuzz");
   } else if (i % 3 == 0) {
     console.log("Fizz");
