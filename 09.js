1688deaea0c6089dad33ce0fd20e149bcdc6b969
Farya <76533997+Xfarya@users.noreply.github.com>
2023-07-28 14:12:17 +0200

    added more solutions

--- 09.js
+++ 09.js
@@ -1,9 +1,8 @@
+var x = 4;
+var isThatTrue = false;
 
-var x = 4
-var isThatTrue = false
-
-if ((x = 12) && isThatTrue) {
-    console.log("Javascript is the best!")
+if (x === 12 && isThatTrue) {
+  console.log("Javascript is the best!");
 }
 
-console.log(x)
+console.log(x);
