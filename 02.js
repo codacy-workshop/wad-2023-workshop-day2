8f474c38dcfece389b324ef722a6214eb3198dee
Kiara Kaiser <kiara.kaiser@din-systems.at>
2023-07-28 14:09:58 +0200

    cx

--- 02.js
+++ 02.js
@@ -3,13 +3,13 @@ function getSeason(month) {
     if(month === 1 && month === 2 && month === 12){
         console.log("It's winter.");
     }
-    else if(month === 3 && month === 4 && month === 5){
+    else if(month >= 3 && month <= 5){
         console.log("It's spring.");
     }
-    else if(month === 6 && month === 7 && month === 8){
+    else if(month >= 6 && month <= 8){
         console.log("It's summer.");
     }
-   else if(month === 9 && month === 10 && month === 11){
+   else if(month >= 9 && month <= 11){
         console.log("It's autumn.");
     } else{
        console.log("Invalid month number.");
