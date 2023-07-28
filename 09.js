f19ef1a92bdc946f83fd8e5a738c0f76b9700430
Kiara Kaiser <kiara.kaiser@din-systems.at>
2023-07-28 14:12:22 +0200

    d

--- 09.js
+++ 09.js
@@ -1,9 +1,10 @@
+function test() {
+    var x = 4
+    var isThatTrue = false
 
-var x = 4
-var isThatTrue = false
+    if ((x = 12) && isThatTrue) {
+        console.log("Javascript is the best!")
+    }
 
-if ((x = 12) && isThatTrue) {
-    console.log("Javascript is the best!")
+    console.log(x)
 }
-
-console.log(x)
