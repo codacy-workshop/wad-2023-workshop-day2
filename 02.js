2c00424331732234731990bca32efa4a7286c807
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2023-07-28 14:08:34 +0200

    Fix

--- 02.js
+++ 02.js
@@ -1,18 +1,24 @@
 function getSeason(month) {
+  let message = "Invalid month number.";
   if (month < 1 || month > 12) {
     console.log("Invalid month number.");
     return;
   }
-  if (month >= 9 && month <= 11) {
+  if (isAutumn(month)) {
     console.log("It's autumn.");
-  } else if (month >= 6 && month <= 8) {
+  } else if (isSummer(month)) {
     console.log("It's summer.");
-  } else if (month >= 3 && month <= 5) {
+  } else if (isSpring(month)) {
     console.log("It's spring.");
   } else {
     console.log("It's winter.");
   }
 }
+
+const isAutumn = (month) => month >= 9 && month <= 11;
+const isSummer = (month) => month >= 6 && month <= 8;
+const isSpring = (month) => month >= 3 && month <= 5;
+const isWinter = (month) => month === 12 || month === 1 || month === 2;
 
 const x = 20;
 
