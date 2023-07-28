edc75698dcf3276c3a5302b6d62cda3c8a2260d1
Tom Weiß <weiss.tom@web.de>
2023-07-28 14:04:01 +0200

    01_test

--- 01.js
+++ 01.js
@@ -3,7 +3,7 @@ for (var i = 1; i <= 100; i++) {
     console.log("FizzBuzz");
   } else if (i % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 == 0) {
+  } else if (i % 5 === 0) {
     console.log("Buzz");
   } else {
     console.log(i);
